8ff4b912f1121edc7912ece9fe49545dd3f00aa5
Christopher Robin Chase <christopherchase011@gmail.com>
2024-03-11 21:49:34 +0800

    Moved function to guess.rs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,30 @@
-use rand::Rng;
+mod guess;
+
+use guess::guess;
 use std::io::{self, Write};
 
 fn main() {
-    println!("Welcome to Rust Guessing Game");
+    println!("Welcome to Rust Basics\n\nChoose an option...");
 
-    let mut guess = String::new();
-    let mut rng = rand::thread_rng();
-    let mut act = false;
+    print!("1. Guessing Game\n2. Exit\n> ");
+    let mut _ch = String::new();
+    io::stdout().flush().unwrap();
 
-    while !act {
-        println!("Please enter number to guess:");
+    
+    io::stdin()
+        .read_line(&mut _ch)
+        .expect("Failed to read line");
 
-        io::stdout().flush().unwrap();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let trimmed_guess = guess.trim();
-        if trimmed_guess.is_empty() {
-            println!("Please enter a valid number.");
-            continue;
+    let ch: i32 = match _ch.trim().parse() {
+        Ok(ch) => ch,
+        Err(_) => {
+            println!("Invalid number");
+            return;
         }
+    };
 
-        let parsed_guess = match trimmed_guess.parse::<i32>() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number.");
-                continue;
-            }
-        };
-
-        let random_number = rng.gen_range(1..=10);
-
-        if parsed_guess == random_number {
-            println!("You guessed it right!");
-            act = true;
-        } else {
-            println!("You guessed it wrong! The number was {}\n", random_number);
-        }
-
-        guess.clear();
+    match ch {
+        1 => guess(),
+        _ => println!("Hi there!"),
     }
 }
